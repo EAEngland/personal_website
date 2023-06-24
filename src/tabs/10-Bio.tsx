637e39247fd50c87e6c6394455974872fb273b53
Natasha England-Elbro <nat@natashaee.me>
2023-06-24 16:45:27 +0100

    feat: make exports possible

--- src/tabs/10-Bio.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import { TabInfo } from "../Types";
-
-export const TAB: TabInfo = {
-  render: () => {
-    return <div>Bio goes here</div>;
-  },
-  name: "Biography",
-};
