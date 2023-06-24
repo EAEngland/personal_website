1bda567c3e43ef8d1decdaf9805b5212b85912c0
Natasha England-Elbro <nat@natashaee.me>
2023-06-24 16:27:51 +0100

    feat: update info stuffs

--- src/MainInfo.tsx
+++ src/MainInfo.tsx
@@ -1,8 +1,86 @@
-import { ReactNode } from "react";
+import { ReactNode, ReactPropTypes } from "react";
 import style from "./MainInfo.module.scss";
+import twitterIco from "./assets/icons/twitter.svg";
+import linkedinIco from "./assets/icons/linkedin.svg";
+import orcidIco from "./assets/icons/orcid.svg";
+import rsGateIco from "./assets/icons/researchGate.svg";
+import gsScholarIco from "./assets/icons/googleScholar.svg";
+import Image, { StaticImageData } from "next/image";
 
 function ProfilePic(props: {}) {
   return <div className={style.pfp}>Profile Picture Placeholder</div>;
+}
+function Icon(props: any) {
+  return (
+    <div className={style.icon}>
+      <Image {...props} fill />
+    </div>
+  );
+}
+interface ExternalContact {
+  icon: StaticImageData | string;
+  href: string;
+  text: string;
+  alt: string;
+}
+const EXTERNAL_CONTACTS: ExternalContact[] = [
+  {
+    icon: twitterIco,
+    alt: "Twitter",
+    href: "https://twitter.com/EdithAEngland",
+    text: "@EdithAEngland",
+  },
+  {
+    icon: linkedinIco,
+    alt: "LinkedIn",
+    href: "https://www.linkedin.com/in/edithaengland/",
+    text: "edithaengland",
+  },
+  {
+    icon: rsGateIco,
+    alt: "",
+    href: "https://www.researchgate.net/profile/Edith-England-2",
+    text: "Research Gate",
+  },
+  {
+    icon: orcidIco,
+    alt: "",
+    href: "https://orcid.org/0000-0001-9894-8323",
+    text: "ORCID",
+  },
+  {
+    icon: gsScholarIco,
+    alt: "Google Scholar",
+    href: "https://scholar.google.com/citations?user=6Y0WXtwAAAAJ",
+    text: "Edith England",
+  },
+];
+
+function ExternalContacts() {
+  const contactEls = EXTERNAL_CONTACTS.map((c) => (
+    <div className={style.externalContact} key={c.alt}>
+      <Icon src={c.icon} alt={c.alt} />
+      <a href={c.href}>{c.text}</a>
+    </div>
+  ));
+  return (
+    <div className={style.externalContacts}>
+      <h4 className={style.contactHeader}>External</h4>
+      <hr />
+      <div>
+        <div>{contactEls}</div>
+      </div>
+    </div>
+  );
+}
+function ContactDetails() {
+  return (
+    <div className={style.contacts}>
+      <div>email</div>
+      <div>telephone</div>
+      <div>etc</div>
+    </div>
+  );
 }
 
 export default function MainInfo(props: {}) {
@@ -15,12 +93,10 @@ export default function MainInfo(props: {}) {
           <h4>Lecturer</h4>
           <h4>School of etc</h4>
         </div>
-        <div className={style.contacts}>
-          <div>email</div>
-          <div>telephone</div>
-          <div>etc</div>
-        </div>
+        <ContactDetails />
       </div>
+      <div className={style.flexFiller} />
+      <ExternalContacts />
     </div>
   );
 }
