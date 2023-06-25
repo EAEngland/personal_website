3455ee222785950e2ef4cb35621f672a0a176c26
EAEngland <EdithAEngland@gmail.com>
2023-06-25 13:24:47 +0100

    Update MoreBtn.tsx

--- src/MoreBtn.tsx
+++ src/MoreBtn.tsx
@@ -6,7 +6,7 @@ export default function MoreBtn(props: {
 }) {
   return (
     <button className={style.moreBtn} onClick={props.onClick}>
-      <i>«{props.active ? "hide" : "click for more"}»</i>
+      <i>«{props.active ? "hide" : "read more"}»</i>
     </button>
   );
 }
